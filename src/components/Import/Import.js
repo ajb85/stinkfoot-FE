df8793352f6414896f6bdfdc7e8229b95af84271
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-03-29 21:02:07 -0400

    Saves build to state

--- src/components/Import/Import.js
+++ src/components/Import/Import.js
@@ -1,17 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+
+import { BuildContext } from 'Providers/Builds.js';
 
 import styles from './styles.module.scss';
 
 function Import(props) {
-  const [build, setBuild] = useState('');
+  const [userBuild, setUserBuild] = useState('');
+  const { saveBuild } = useContext(BuildContext);
+
+  const submitBuild = e => {
+    e.preventDefault();
+
+    saveBuild(userBuild);
+  };
   return (
-    <form className={styles.Import}>
+    <form className={styles.Import} onSubmit={e => submitBuild(e)}>
       <label>
-        Import Build from <b>Import / Export > Short Forum Post</b>. Select
-        "HTML Export" from the right menu then click "Export Now". Paste the
-        results in the box below.
+        Import Build from <b>Import / Export > Long Forum Post</b>. Select "HTML
+        Export" from the right menu then click "Export Now". Paste the results
+        in the box below.
       </label>
-      <textarea value={build} onChange={e => setBuild(e.target.value)} />
+      <textarea
+        value={userBuild}
+        onChange={e => setUserBuild(e.target.value)}
+      />
       <button type="submit">Submit Build</button>
     </form>
   );
