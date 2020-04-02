4b8ca07e04b8c36aa01af70710c7c072134fe3d7
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-04-01 20:33:26 -0400

    Changes FilterOptions to be fixed and adds a block to hold its place

--- src/components/BuildList/BuildList.js
+++ src/components/BuildList/BuildList.js
@@ -3,6 +3,8 @@ import React, { useState, useEffect, useContext } from 'react';
 import { BuildContext } from 'Providers/Builds.js';
 import FilterOptions from './FilterOptions.js';
 
+import categoryName from 'js/categories.js';
+
 import styles from './styles.module.scss';
 
 function ListBuild(props) {
@@ -29,48 +31,6 @@ function ListBuild(props) {
   const { build } = useContext(BuildContext);
 
   useEffect(() => {
-    const control = {
-      name: 'Control',
-      color:
-        'linear-gradient(0deg, rgba(123,70,2,1) 0%, rgba(252,142,0,1) 50%, rgba(255,202,132,1) 100%)',
-      activeColor: 'rgb(123,70,2)'
-    };
-    const categoryName = {
-      defense: {
-        name: 'Def',
-        color:
-          'linear-gradient(0deg, rgba(64,53,89,1) 0%, rgba(123,102,173,1) 50%, rgba(188,176,216,1) 100%)',
-        activeColor: 'rgba(64,53,89,1)'
-      },
-      resistance: {
-        name: 'Res',
-        color:
-          'linear-gradient(0deg, rgba(89,53,69,1) 0%, rgba(173,102,134,1) 50%, rgba(220,184,200,1) 100%)',
-        activeColor: 'rgb(89,53,69)'
-      },
-      slow: {
-        name: 'Slow',
-        color:
-          'linear-gradient(0deg, rgba(17,50,40,1) 0%, rgba(33,97,78,1) 50%, rgba(149,179,170,1) 100%)',
-        activeColor: 'rgb(17,50,40)'
-      },
-      damage: {
-        name: 'Dmg',
-        color:
-          'linear-gradient(0deg, rgba(122,0,0,1) 0%, rgba(252,0,0,1) 50%, rgba(255,132,132,1) 100%)',
-        activeColor: 'rgb(122,0,0)'
-      },
-      healing: {
-        name: 'Heal',
-        color:
-          'linear-gradient(0deg, rgba(0,114,1,1) 0%, rgba(12,225,15,1) 50%, rgba(132,240,134,1) 100%)',
-        activeColor: 'rgb(0,114,1)'
-      },
-      hold: control,
-      sleep: control,
-      stun: control,
-      disorient: control
-    };
     const categoryList = new Set();
     for (let setName in build) {
       for (let eName in build[setName]) {
@@ -89,8 +49,25 @@ function ListBuild(props) {
   return (
     <div className={styles.BuildList}>
       <FilterOptions filters={filters} toggleTag={toggleTag} />
+      <div className={styles.block} />
       <div className={styles.list}>
         {Object.entries(build)
+          .filter(([_, enhancements]) => {
+            if (!filters.tags.length) {
+              return true;
+            }
+            for (let e in enhancements) {
+              const stats = e.split('/');
+              for (let i = 0; i < stats.length; i++) {
+                const s = stats[i];
+                const category = categoryName[s.toLowerCase()];
+                if (category && filters.tags[category.name]) {
+                  return true;
+                }
+              }
+            }
+            return false;
+          })
           .sort((a, b) => {
             const countA = Object.keys(a[1]).length;
             const countB = Object.keys(b[1]).length;
