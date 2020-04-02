0518de9fc4c94d20b3b4183afe6bde3e6aac6944
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-04-01 21:03:02 -0400

    Adds consistent size to buttons, allows wrap, and removes space-between

--- src/components/BuildList/FilterOptions.js
+++ src/components/BuildList/FilterOptions.js
@@ -5,20 +5,23 @@ import styles from './styles.module.scss';
 function FilterOptions({ filters, toggleTag }) {
   return (
     <div className={styles.FilterButtons}>
-      {filters.options.map(o => {
-        const isActive = !!filters.tags[o.name];
-        const background = isActive ? o.activeColor : o.color;
-        return (
-          <button
-            key={o.name}
-            type="button"
-            style={{ background }}
-            onClick={toggleTag.bind(this, o.name)}
-          >
-            {o.name}
-          </button>
-        );
-      })}
+      <label>Filter by Enhancement Stat</label>
+      <div className={styles.statButtons}>
+        {filters.options.map(o => {
+          const isActive = !!filters.tags[o.name];
+          const background = isActive ? o.activeColor : o.color;
+          return (
+            <button
+              key={o.name}
+              type="button"
+              style={{ background }}
+              onClick={toggleTag.bind(this, o.name)}
+            >
+              {o.name}
+            </button>
+          );
+        })}
+      </div>
     </div>
   );
 }
