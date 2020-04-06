de301214389cf9593393dbaafb2ea7845f77209e
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-04-05 21:10:21 -0400

    Updates textarea to input and automatically attempts to submit onChange.  Additionally, error texts displays with instructions if it fails

--- src/components/Import/Import.js
+++ src/components/Import/Import.js
@@ -1,30 +1,56 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useEffect, useRef, useContext } from 'react';
 
 import { BuildContext } from 'Providers/Builds.js';
 
 import styles from './styles.module.scss';
 
 function Import(props) {
-  const [userBuild, setUserBuild] = useState('');
+  const inputREF = useRef(null);
+  const [wasSubmitted, setWasSubmitted] = useState(false);
   const { saveBuild } = useContext(BuildContext);
+
+  useEffect(() => {
+    if (inputREF.current) {
+      inputREF.current.focus();
+    }
+  }, [inputREF]);
 
   const submitBuild = e => {
     e.preventDefault();
-
-    saveBuild(userBuild);
+    saveBuild(e.target.value);
+    setWasSubmitted(true);
   };
   return (
-    <form className={styles.Import} onSubmit={e => submitBuild(e)}>
+    <form className={styles.Import}>
       <label>
         In your builder, go to <b>Import / Export > Long Forum Export</b>.
         Select "HTML Export" from the right menu then click "Export Now". Paste
         the results in the box below.
       </label>
-      <textarea
-        value={userBuild}
-        onChange={e => setUserBuild(e.target.value)}
+      <input
+        ref={inputREF}
+        onChange={e => submitBuild(e)}
+        // style={{ display: 'none' }}
       />
-      <button type="submit">Submit Build</button>
+      {wasSubmitted && (
+        <div className={styles.errorText}>
+          <p>
+            Hmm, that's not a build format I understand. Be sure to follow these
+            steps:
+          </p>
+          <ul>
+            <li>Open your Mids or Pines hero planner</li>
+            <li>Ensure you have IO Sets equipped on your build</li>
+            <li>Click "Import/Export" at the top</li>
+            <li>Click "Long Forum Export..."</li>
+            <li>
+              In the menu that opens, select "HTML Export" from the right side
+            </li>
+            <li>Click the "Export Now" button at the bottom</li>
+            <li>Paste those results into the input above</li>
+          </ul>
+        </div>
+      )}
     </form>
   );
 }
