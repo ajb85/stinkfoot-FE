546abc2ec8d6a1ee784a2701591aa71e7d1c2998
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-04-12 00:32:42 -0400

    Updates to use new parser and while losing data, updates localStorage to a multi-build pattern

--- src/Providers/Builds.js
+++ src/Providers/Builds.js
@@ -1,22 +1,28 @@
 import React, { useState, createContext } from 'react';
 
+import parseStringToBuild from 'js/parseStringToBuild.js';
+
 export const BuildContext = createContext();
 
 function BuildProvider(props) {
-  const currentBuild = updateOldDataStructures(
-    localStorage.getItem('currentBuild')
-  );
+  const storage = localStorage.getItem('builds');
+  const builds = storage ? JSON.parse(storage).active : {};
+  const [build, setBuild] = useState(builds ? builds : {});
 
-  const [build, setBuild] = useState(currentBuild ? currentBuild : {});
+  const _saveBuildToActive = (build) => {
+    const builds = localStorage.getItem('builds') || {};
+
+    builds.active = build;
+    localStorage.setItem('builds', JSON.stringify(builds));
+    setBuild(builds.active);
+  };
 
   const saveBuild = (str) => {
     if (!str) {
       setBuild({});
       return;
     }
-    const content = parseStringIntoContent(str);
-    localStorage.setItem('currentBuild', JSON.stringify(content));
-    setBuild(content);
+    _saveBuildToActive(parseStringToBuild(str));
   };
 
   const _saveUpdatedBuild = (newEnh, setName) => {
@@ -35,8 +41,7 @@ function BuildProvider(props) {
       completed: isComplete,
     };
 
-    localStorage.setItem('currentBuild', JSON.stringify(newBuild));
-    setBuild(newBuild);
+    _saveBuildToActive(newBuild);
   };
 
   const toggleEnhancement = (setName, enhName) => {
@@ -74,8 +79,7 @@ function BuildProvider(props) {
     }
 
     const newBuild = { ...build, [setName]: { ...setCopy, enhancements } };
-    localStorage.setItem('currentBuild', JSON.stringify(newBuild));
-    setBuild(newBuild);
+    _saveBuildToActive(newBuild);
   };
 
   const { Provider } = BuildContext;
@@ -89,113 +93,3 @@ function BuildProvider(props) {
 }
 
 export default BuildProvider;
-
-function parseStringIntoContent(str) {
-  const content = [];
-  for (let i = 0; i < str.length; i++) {
-    const char = str[i];
-
-    if (char === '<' && str.substring(i, i + 5) === '<font') {
-      const distanceToClosingBracket = str.substring(i + 5).indexOf('>');
-      const contentStart = i + 6 + distanceToClosingBracket;
-      const distanceToClosingTag = str
-        .substring(contentStart)
-        .indexOf('</font>');
-      const distanceToEndClosingTag = str
-        .substring(contentStart + distanceToClosingTag)
-        .indexOf('>');
-      const contentStr = str.substring(
-        contentStart,
-        contentStart + distanceToClosingTag
-      );
-      if (isValidContent(contentStr)) {
-        content.push(contentStr);
-        i = contentStart + distanceToClosingTag + distanceToEndClosingTag;
-      }
-    }
-  }
-
-  return formatData(content);
-}
-
-function isValidContent(str) {
-  const badContentStarts = {
-    '<': true,
-    ':': true,
-  };
-  return !badContentStarts[str[0]] && str.substring(0, 5) !== 'Level';
-}
-
-function formatData(arr) {
-  const updated = {};
-
-  for (let i = 4; i < arr.length; i++) {
-    const content = arr[i];
-    if (content === 'Power Pool: ') {
-      i += 1;
-    } else if (content.indexOf(' - ') > -1) {
-      const setName = content.substring(0, content.indexOf(' - '));
-      if (updated[setName]) {
-        const setEnhancements = updated[setName].enhancements;
-        if (setEnhancements[arr[i + 1]]) {
-          setEnhancements[arr[i + 1]].need++;
-        } else {
-          setEnhancements[arr[i + 1]] = { need: 1, have: 0 };
-        }
-      } else {
-        updated[setName] = {
-          enhancements: { [arr[i + 1]]: { need: 1, have: 0 } },
-          completed: false,
-        };
-      }
-      i += 1;
-    }
-  }
-  return updated;
-}
-
-function updateOldDataStructures(build) {
-  if (!build) {
-    return null;
-  }
-
-  if (typeof build === 'string') {
-    build = JSON.parse(build);
-  }
-
-  if (build.hasOwnProperty('completed')) {
-    delete build.completed;
-  }
-
-  for (let setName in build) {
-    const set = build[setName];
-    if (!set.enhancements) {
-      return _modernizeStructure(build);
-    }
-  }
-
-  return build;
-}
-
-function _modernizeStructure(build) {
-  // Assumes build exists & is an object
-
-  const buildCopy = { ...build };
-  const updatedBuild = {};
-
-  for (let setName in buildCopy) {
-    const _set = buildCopy[setName];
-    const { completed, ...enhancements } = _set;
-    const updatedSet = { enhancements, completed };
-
-    for (let eName in enhancements) {
-      const enh = enhancements[eName];
-      const { completed, count: need } = enh;
-
-      updatedSet.enhancements[eName] = { need, have: completed ? need : 0 };
-    }
-    updatedBuild[setName] = updatedSet;
-  }
-
-  return updatedBuild;
-}
