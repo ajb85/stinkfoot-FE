c511d323d78730b488e841a81d698956222d5325
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-04-19 21:10:17 -0400

    Adds archetype data

---
+++ src/data/origins.js
@@ -0,0 +1,7 @@
+export default [
+  { name: 'Magic', DO: 'MagDO', SO: 'MagSO' },
+  { name: 'Mutation', DO: 'MutDO', SO: 'MutSO' },
+  { name: 'Natural', DO: 'NatDO', SO: 'NatSO' },
+  { name: 'Science', DO: 'SciDO', SO: 'SciSO' },
+  { name: 'Technology', DO: 'TechDO', SO: 'TechSO' },
+];
