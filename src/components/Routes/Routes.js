1a2c40fbf3d58a4770d389369c6bc4ad9d869f01
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-04-19 21:10:58 -0400

    Adds routes for planner/shopping

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -1,13 +1,26 @@
 import React, { useContext } from 'react';
-// import { Route } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Import from 'components/Import/';
 import BuildList from 'components/BuildList/';
+import Planner from 'Planner/';
 
 import { BuildContext } from 'Providers/Builds.js';
 
 function Routes(props) {
   const { build } = useContext(BuildContext);
-  return <>{Object.keys(build).length ? <BuildList /> : <Import />}</>;
+  return (
+    <Switch>
+      <Route path="/planner">
+        <Planner />
+      </Route>
+      <Route path="/shopper">
+        {Object.keys(build).length ? <BuildList /> : <Import />}
+      </Route>
+      <Route path="/">
+        <Redirect to="/planner" />
+      </Route>
+    </Switch>
+  );
 }
 
 export default Routes;
