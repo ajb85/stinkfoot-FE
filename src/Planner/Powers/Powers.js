f4202222d201033a8aca2edf77675a9b244e76c2
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-04-21 15:17:24 -0400

    Renders enhancement slots.  Clicking power name adds slot, clicking slot removes it

--- src/Planner/Powers/Powers.js
+++ src/Planner/Powers/Powers.js
@@ -1,34 +1,56 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
-function Powers({ build, setActiveLevel }) {
+function Powers({ build, setActiveLevel, addSlot, removeSlot }) {
   return (
     <div>
-      {build.powers.map((p, i) => {
-        const { level, name /*, slots, type*/ } = p;
+      {build.powerSlots.map((powerSlot, i) => {
+        const { level, name, enhSlots } = powerSlot;
         const isActive = build.activeLevel === level;
-        const isEmpty = !name;
+        const isEmpty = !powerSlot.name;
+
         return (
-          <div
-            onClick={() => {
-              if (isEmpty) {
-                setActiveLevel(level);
-              }
-            }}
-            style={{
-              backgroundColor: name
-                ? isActive
-                  ? 'lightblack'
-                  : 'black'
-                : isActive
-                ? 'green'
-                : 'darkblue',
-            }}
-            key={i}
-          >
-            ({level}) {name}
-          </div>
+          <Fragment key={i}>
+            {isEmpty ? (
+              <EmptyPowerSlot
+                setActiveLevel={setActiveLevel}
+                level={powerSlot.level}
+                isActive={build.activeLevel === powerSlot.level}
+              />
+            ) : (
+              <div
+                style={{
+                  backgroundColor: isActive ? 'lightblack' : 'black',
+                }}
+              >
+                <p onClick={addSlot.bind(this, i)}>
+                  ({level}) {name}
+                </p>
+                <div>
+                  {enhSlots.map(({ slotLevel, setName, name }, j) => {
+                    const displayLevel = slotLevel === null ? level : slotLevel;
+                    return (
+                      <p onClick={removeSlot.bind(this, i, j)} key={j}>
+                        {displayLevel}
+                      </p>
+                    );
+                  })}
+                </div>
+              </div>
+            )}
+          </Fragment>
         );
       })}
+    </div>
+  );
+}
+
+function EmptyPowerSlot({ level, isActive, setActiveLevel }) {
+  return (
+    <div
+      onClick={setActiveLevel.bind(this, level)}
+      style={{ backgroundColor: isActive ? 'green' : 'darkblue' }}
+    >
+      <p>({level})</p>
     </div>
   );
 }
