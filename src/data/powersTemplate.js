5f90c496f61bc3c2287f157ca3493645f90ba31b
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-04-20 20:41:40 -0400

    Empty templte for powers, initialstate

---
+++ src/data/powersTemplate.js
@@ -0,0 +1,180 @@
+export default [
+  {
+    level: 1,
+    type: 'selected',
+  },
+  {
+    level: 1,
+    type: 'selected',
+  },
+  {
+    level: 2,
+    type: 'selected',
+  },
+  {
+    level: 4,
+    type: 'selected',
+  },
+  {
+    level: 6,
+    type: 'selected',
+  },
+  {
+    level: 8,
+    type: 'selected',
+  },
+  {
+    level: 10,
+    type: 'selected',
+  },
+  {
+    level: 12,
+    type: 'selected',
+  },
+  {
+    level: 14,
+    type: 'selected',
+  },
+  {
+    level: 16,
+    type: 'selected',
+  },
+  {
+    level: 18,
+    type: 'selected',
+  },
+  {
+    level: 20,
+    type: 'selected',
+  },
+  {
+    level: 22,
+    type: 'selected',
+  },
+  {
+    level: 24,
+    type: 'selected',
+  },
+  {
+    level: 26,
+    type: 'selected',
+  },
+  {
+    level: 28,
+    type: 'selected',
+  },
+  {
+    level: 30,
+    type: 'selected',
+  },
+  {
+    level: 32,
+    type: 'selected',
+  },
+  {
+    level: 35,
+    type: 'selected',
+  },
+  {
+    level: 38,
+    type: 'selected',
+  },
+  {
+    level: 41,
+    type: 'selected',
+  },
+  {
+    level: 44,
+    type: 'selected',
+  },
+  {
+    level: 47,
+    type: 'selected',
+  },
+  {
+    level: 49,
+    type: 'selected',
+  },
+  {
+    level: 1,
+    name: 'Brawl',
+    slots: [
+      {
+        slotLevel: null,
+      },
+    ],
+    type: 'default',
+  },
+  // {
+  //   level: 1,
+  //   name: 'Scourge',
+  //   slots: [],
+  //   type: 'default',
+  // },
+  {
+    level: 1,
+    name: 'Sprint',
+    slots: [
+      {
+        slotLevel: null,
+      },
+    ],
+    type: 'default',
+  },
+  {
+    level: 2,
+    name: 'Rest',
+    slots: [
+      {
+        slotLevel: null,
+      },
+    ],
+    type: 'default',
+  },
+  {
+    level: 4,
+    name: 'Ninja Run',
+    slots: [],
+    type: 'default',
+  },
+  {
+    level: 2,
+    name: 'Swift',
+    slots: [
+      {
+        slotLevel: null,
+      },
+    ],
+    type: 'default',
+  },
+  {
+    level: 2,
+    name: 'Health',
+    slots: [
+      {
+        slotLevel: null,
+      },
+    ],
+    type: 'default',
+  },
+  {
+    level: 2,
+    name: 'Hurdle',
+    slots: [
+      {
+        slotLevel: null,
+      },
+    ],
+    type: 'default',
+  },
+  {
+    level: 2,
+    name: 'Stamina',
+    slots: [
+      {
+        slotLevel: null,
+      },
+    ],
+    type: 'default',
+  },
+];
