9a4e2961d6977ed23be1e9e3c46b0be0c0d5de0a
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-04-21 19:56:01 -0400

    Adds basic styles

--- src/Planner/Powersets/Powersets.js
+++ src/Planner/Powersets/Powersets.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 import powersets from 'data/powersets.js';
 
+import styles from './styles.module.scss';
+
 function Powersets({ build, updateBuild, togglePower }) {
   const { primary, secondary, archetype } = build;
   const { primaries, secondaries } = powersets[archetype];
 
   const renderPowerset = (set, isPrimary) => {
     return (
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <div className={styles.powersList}>
         {set.powers
           .filter(({ isEpic }) => !isEpic)
           .map((p) => {
@@ -33,47 +35,52 @@ function Powersets({ build, updateBuild, togglePower }) {
     );
   };
   return (
-    <>
+    <section className={styles.Powersets}>
       <div>
-        <label>Primary Power Set</label>
-        <select
-          value={primary.displayName}
-          name="primary"
-          onChange={(e) => updateBuild(e)}
-        >
-          {primaries.map((p) => (
-            <option key={p.displayName} value={p.displayName}>
-              {p.displayName}
-            </option>
-          ))}
-        </select>
-        <label>Secondary Power Set</label>
-        <select
-          value={secondary.displayName}
-          name="secondary"
-          onChange={(e) => updateBuild(e)}
-        >
-          {secondaries.map((p) => (
-            <option key={p.displayName} value={p.displayName}>
-              {p.displayName}
-            </option>
-          ))}
-        </select>
+        {/* <h2>Powersets</h2> */}
+        <div className={styles.powersetContainer}>
+          <div className={styles.powerset}>
+            <h3>Primary</h3>
+            <select
+              value={primary.displayName}
+              name="primary"
+              onChange={(e) => updateBuild(e)}
+            >
+              {primaries.map((p) => (
+                <option key={p.displayName} value={p.displayName}>
+                  {p.displayName}
+                </option>
+              ))}
+            </select>
+            {renderPowerset(
+              primaries.find(
+                ({ displayName }) => displayName === primary.displayName
+              ),
+              true
+            )}
+          </div>
+          <div className={styles.powerset}>
+            <h3>Secondary</h3>
+            <select
+              value={secondary.displayName}
+              name="secondary"
+              onChange={(e) => updateBuild(e)}
+            >
+              {secondaries.map((p) => (
+                <option key={p.displayName} value={p.displayName}>
+                  {p.displayName}
+                </option>
+              ))}
+            </select>
+            {renderPowerset(
+              secondaries.find(
+                ({ displayName }) => displayName === secondary.displayName
+              )
+            )}
+          </div>
+        </div>
       </div>
-      <div style={{ display: 'flex' }}>
-        {renderPowerset(
-          primaries.find(
-            ({ displayName }) => displayName === primary.displayName
-          ),
-          true
-        )}
-        {renderPowerset(
-          secondaries.find(
-            ({ displayName }) => displayName === secondary.displayName
-          )
-        )}
-      </div>
-    </>
+    </section>
   );
 }
 
