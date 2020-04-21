fba268072f9483dacf81018b93929d768fbfff17
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-04-20 20:42:09 -0400

    Adds method (and supporting functions) to toggle a power in the build

--- src/Planner/Planner.js
+++ src/Planner/Planner.js
@@ -2,9 +2,12 @@ import React, { useState } from 'react';
 
 import SelectAT from './SelectAT/';
 import Powersets from './Powersets/';
+import Powers from './Powers/';
 
 import powersets from 'data/powersets.js';
 import origins from 'data/origins.js';
+import powersTemplate from 'data/powersTemplate.js';
+import slots from 'data/slots.js';
 
 function Planner(props) {
   const [build, setBuild] = useState({
@@ -14,12 +17,11 @@ function Planner(props) {
     alignment: 'Hero',
     primary: powersets.Blaster.primaries[0],
     secondary: powersets.Blaster.secondaries[0],
+    powers: powersTemplate,
+    slots,
+    activeLevel: 1,
+    powerLookup: {},
   });
-
-  // const [powersets, setPowersets] = useState({
-  //   primary: { length: 0 },
-  //   secondary: { length: 0 },
-  // });
 
   console.log('BUILD: ', build);
 
@@ -43,14 +45,133 @@ function Planner(props) {
     }
   };
 
-  // const loadFile = () => {};
+  const _findNextActive = (assignedLevel = build.activeLevel) => {
+    const nextLevel = build.powers.find(
+      ({ level, name }) => level > assignedLevel && !name
+    );
+    if (nextLevel) {
+      return nextLevel.level;
+    }
+
+    const firstEmpty = build.powers.find(({ name }) => !name);
+
+    return firstEmpty ? firstEmpty.level : null;
+  };
+
+  const _assignLevel = (powersLevel) => {
+    if (powersLevel <= build.activeLevel) {
+      return build.activeLevel;
+    }
+
+    const nextAbovePowersLevel = build.powers.find(
+      ({ level, name }) => level >= powersLevel && !name
+    );
+    return nextAbovePowersLevel ? nextAbovePowersLevel.level : null;
+  };
+
+  const togglePower = (p, isPrimary) => {
+    if (build.powerLookup.hasOwnProperty(p.displayName)) {
+      // Remove power that's been added
+      const powerLookup = { ...build.powerLookup };
+      const index = powerLookup[p.displayName];
+      delete powerLookup[p.displayName];
+
+      let activeLevel;
+      const powers = build.powers.map((powerSlot, i) => {
+        if (i !== index || powerSlot.type === 'default') {
+          return powerSlot;
+        }
+        activeLevel = powerSlot.level;
+        const { level, type } = powerSlot;
+        return { level, type };
+      });
+      setBuild({
+        ...build,
+        powerLookup,
+        powers,
+        activeLevel,
+      });
+    } else {
+      if (p.level === 1 && !build.powers[1].name) {
+        const newPowers = [...build.powers];
+        const powerLookup = { ...build.powerLookup };
+        if (isPrimary) {
+          newPowers[0] = {
+            ...newPowers[0],
+            name: p.displayName,
+            slots: emptyDefaultSlot(),
+          };
+          powerLookup[p.displayName] = 0;
+        }
+        if (!newPowers[1].name) {
+          const powerName = build.secondary.powers.find(({ isEpic }) => !isEpic)
+            .displayName;
+          newPowers[1] = {
+            ...newPowers[1],
+            name: powerName,
+            slots: emptyDefaultSlot(),
+          };
+          powerLookup[powerName] = 1;
+        }
+        setBuild({
+          ...build,
+          powers: newPowers,
+          powerLookup,
+          activeLevel: _findNextActive(),
+        });
+      } else {
+        let newIndex;
+        const assignedLevel = _assignLevel(p.level);
+        if (assignedLevel === null) {
+          return;
+        }
+
+        const powers = build.powers.map((powerSlot, i) => {
+          if (
+            powerSlot.level !== assignedLevel ||
+            powerSlot.type === 'default'
+          ) {
+            return powerSlot;
+          }
+          newIndex = i;
+          return {
+            ...powerSlot,
+            name: p.displayName,
+            slots: emptyDefaultSlot(),
+          };
+        });
+        setBuild({
+          ...build,
+          powers,
+          powerLookup: {
+            ...build.powerLookup,
+            [p.displayName]: newIndex,
+          },
+          activeLevel: _findNextActive(assignedLevel),
+        });
+      }
+    }
+  };
 
   return (
     <div>
       <SelectAT build={build} updateBuild={updateBuild} setBuild={setBuild} />
-      <Powersets build={build} updateBuild={updateBuild} />
+      <Powersets
+        build={build}
+        updateBuild={updateBuild}
+        togglePower={togglePower}
+      />
+      <Powers build={build} />
     </div>
   );
+}
+
+function emptyDefaultSlot() {
+  return [
+    {
+      slotLevel: null,
+    },
+  ];
 }
 
 export default Planner;
