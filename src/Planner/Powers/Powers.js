fc2e035e617d809849f4160c5414081555090141
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-04-20 23:42:19 -0400

    Active level can be set by clicking a level now

--- src/Planner/Powers/Powers.js
+++ src/Planner/Powers/Powers.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
-function Powers({ build }) {
+function Powers({ build, setActiveLevel }) {
   return (
     <div>
       {build.powers.map((p, i) => {
         const { level, name /*, slots, type*/ } = p;
         const isActive = build.activeLevel === level;
+        const isEmpty = !name;
         return (
           <div
+            onClick={() => {
+              if (isEmpty) {
+                setActiveLevel(level);
+              }
+            }}
             style={{
               backgroundColor: name
                 ? isActive
