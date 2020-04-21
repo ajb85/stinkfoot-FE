705f32e97fb034dfd06b692179e396743555ec06
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-04-21 15:18:07 -0400

    Renames file to have a clearer name

---
+++ src/data/enhancementSlots.js
@@ -0,0 +1,264 @@
+export default [
+  { value: 3, inUse: false },
+  {
+    value: 3,
+    inUse: false,
+  },
+  {
+    value: 5,
+    inUse: false,
+  },
+  {
+    value: 5,
+    inUse: false,
+  },
+  {
+    value: 7,
+    inUse: false,
+  },
+  {
+    value: 7,
+    inUse: false,
+  },
+  {
+    value: 9,
+    inUse: false,
+  },
+  {
+    value: 9,
+    inUse: false,
+  },
+  {
+    value: 11,
+    inUse: false,
+  },
+  {
+    value: 11,
+    inUse: false,
+  },
+  {
+    value: 13,
+    inUse: false,
+  },
+  {
+    value: 13,
+    inUse: false,
+  },
+  {
+    value: 15,
+    inUse: false,
+  },
+  {
+    value: 15,
+    inUse: false,
+  },
+  {
+    value: 17,
+    inUse: false,
+  },
+  {
+    value: 17,
+    inUse: false,
+  },
+  {
+    value: 19,
+    inUse: false,
+  },
+  {
+    value: 19,
+    inUse: false,
+  },
+  {
+    value: 21,
+    inUse: false,
+  },
+  {
+    value: 21,
+    inUse: false,
+  },
+  {
+    value: 23,
+    inUse: false,
+  },
+  {
+    value: 23,
+    inUse: false,
+  },
+  {
+    value: 25,
+    inUse: false,
+  },
+  {
+    value: 25,
+    inUse: false,
+  },
+  {
+    value: 27,
+    inUse: false,
+  },
+  {
+    value: 27,
+    inUse: false,
+  },
+  {
+    value: 29,
+    inUse: false,
+  },
+  {
+    value: 29,
+    inUse: false,
+  },
+  {
+    value: 31,
+    inUse: false,
+  },
+  {
+    value: 31,
+    inUse: false,
+  },
+  {
+    value: 31,
+    inUse: false,
+  },
+  {
+    value: 33,
+    inUse: false,
+  },
+  {
+    value: 33,
+    inUse: false,
+  },
+  {
+    value: 33,
+    inUse: false,
+  },
+  {
+    value: 34,
+    inUse: false,
+  },
+  {
+    value: 34,
+    inUse: false,
+  },
+  {
+    value: 34,
+    inUse: false,
+  },
+  {
+    value: 36,
+    inUse: false,
+  },
+  {
+    value: 36,
+    inUse: false,
+  },
+  {
+    value: 36,
+    inUse: false,
+  },
+  {
+    value: 37,
+    inUse: false,
+  },
+  {
+    value: 37,
+    inUse: false,
+  },
+  {
+    value: 37,
+    inUse: false,
+  },
+  {
+    value: 39,
+    inUse: false,
+  },
+  {
+    value: 39,
+    inUse: false,
+  },
+  {
+    value: 39,
+    inUse: false,
+  },
+  {
+    value: 40,
+    inUse: false,
+  },
+  {
+    value: 40,
+    inUse: false,
+  },
+  {
+    value: 40,
+    inUse: false,
+  },
+  {
+    value: 42,
+    inUse: false,
+  },
+  {
+    value: 42,
+    inUse: false,
+  },
+  {
+    value: 42,
+    inUse: false,
+  },
+  {
+    value: 43,
+    inUse: false,
+  },
+  {
+    value: 43,
+    inUse: false,
+  },
+  {
+    value: 43,
+    inUse: false,
+  },
+  {
+    value: 45,
+    inUse: false,
+  },
+  {
+    value: 45,
+    inUse: false,
+  },
+  {
+    value: 45,
+    inUse: false,
+  },
+  {
+    value: 46,
+    inUse: false,
+  },
+  {
+    value: 46,
+    inUse: false,
+  },
+  {
+    value: 46,
+    inUse: false,
+  },
+  {
+    value: 48,
+    inUse: false,
+  },
+  {
+    value: 48,
+    inUse: false,
+  },
+  {
+    value: 48,
+    inUse: false,
+  },
+  {
+    value: 50,
+    inUse: false,
+  },
+  {
+    value: 50,
+    inUse: false,
+  },
+  { value: 50, inUse: false },
+];
