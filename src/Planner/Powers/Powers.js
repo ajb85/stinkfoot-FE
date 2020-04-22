cdb8822a8e5358cfee78fd41ee83b67319fdcc7e
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-04-21 21:00:01 -0400

    Renders enhancement slots as bubbles

--- src/Planner/Powers/Powers.js
+++ src/Planner/Powers/Powers.js
@@ -1,45 +1,76 @@
 import React, { Fragment } from 'react';
 
-function Powers({ build, setActiveLevel, addSlot, removeSlot }) {
-  return (
-    <div>
-      {build.powerSlots.map((powerSlot, i) => {
-        const { level, name, enhSlots } = powerSlot;
-        const isActive = build.activeLevel === level;
-        const isEmpty = !powerSlot.name;
+import styles from './styles.module.scss';
 
-        return (
-          <Fragment key={i}>
-            {isEmpty ? (
-              <EmptyPowerSlot
-                setActiveLevel={setActiveLevel}
-                level={powerSlot.level}
-                isActive={build.activeLevel === powerSlot.level}
-              />
-            ) : (
-              <div
-                style={{
-                  backgroundColor: isActive ? 'lightblack' : 'black',
-                }}
-              >
-                <p onClick={addSlot.bind(this, i)}>
-                  ({level}) {name}
-                </p>
-                <div>
-                  {enhSlots.map(({ slotLevel, setName, name }, j) => {
-                    const displayLevel = slotLevel === null ? level : slotLevel;
-                    return (
-                      <p onClick={removeSlot.bind(this, i, j)} key={j}>
-                        {displayLevel}
-                      </p>
-                    );
-                  })}
-                </div>
-              </div>
-            )}
-          </Fragment>
-        );
-      })}
+function Powers({ build, setActiveLevel, addSlot, removeSlot }) {
+  let index = 0;
+  const { selected, defaults } = build.powerSlots.reduce(
+    (acc, cur) => {
+      if (cur.type === 'default') {
+        acc.defaults.push(cur);
+      } else {
+        if (!acc.selected[index]) {
+          acc.selected.push([]);
+        }
+
+        acc.selected[index].push(cur);
+
+        if (acc.selected[index].length >= 8) {
+          index++;
+        }
+      }
+      return acc;
+    },
+    { selected: [], defaults: [] }
+  );
+  return (
+    <div className={styles.Powers}>
+      {selected.map((column) => (
+        <div className={styles.column}>
+          {column.map((powerSlot, i) => {
+            const { level, name, enhSlots } = powerSlot;
+            const isActive = build.activeLevel === level;
+            const isEmpty = !powerSlot.name;
+
+            return (
+              <Fragment key={i}>
+                {isEmpty ? (
+                  <EmptyPowerSlot
+                    setActiveLevel={setActiveLevel}
+                    level={powerSlot.level}
+                    isActive={build.activeLevel === powerSlot.level}
+                  />
+                ) : (
+                  <div
+                    className={styles.power}
+                    style={{
+                      backgroundColor: '#1b4ea8',
+                    }}
+                  >
+                    <p onClick={addSlot.bind(this, i)}>
+                      ({level}) {name}
+                    </p>
+                    <div className={styles.enhancementsContainer}>
+                      {enhSlots.map(({ slotLevel, setName, name }, j) => {
+                        const displayLevel =
+                          slotLevel === null ? level : slotLevel;
+                        return (
+                          <div
+                            onClick={removeSlot.bind(this, i, j)}
+                            className={styles.enhancementBubble}
+                          >
+                            <p key={j}>{displayLevel}</p>
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </div>
+                )}
+              </Fragment>
+            );
+          })}
+        </div>
+      ))}
     </div>
   );
 }
@@ -47,8 +78,9 @@ function Powers({ build, setActiveLevel, addSlot, removeSlot }) {
 function EmptyPowerSlot({ level, isActive, setActiveLevel }) {
   return (
     <div
+      className={styles.power}
       onClick={setActiveLevel.bind(this, level)}
-      style={{ backgroundColor: isActive ? 'green' : 'darkblue' }}
+      style={{ backgroundColor: isActive ? 'green' : 'grey' }}
     >
       <p>({level})</p>
     </div>
