2f186898b328838357974bd8fc19f6f7c9315218
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-04-23 16:16:31 -0400

    Extracted data from Mids, updated to match this app

---
+++ src/data/poolPowers.js
@@ -0,0 +1,25 @@
+import Concealment from 'data/power_pools/Concealment.json';
+import Fighting from 'data/power_pools/Fighting.json';
+import Flight from 'data/power_pools/Flight.json';
+import Force_of_Will from 'data/power_pools/Force_of_Will.json';
+import Leadership from 'data/power_pools/Leadership.json';
+import Leaping from 'data/power_pools/Leaping.json';
+import Medicine from 'data/power_pools/Medicine.json';
+import Presence from 'data/power_pools/Presence.json';
+import Sorcery from 'data/power_pools/Sorcery.json';
+import Speed from 'data/power_pools/Speed.json';
+import Teleportation from 'data/power_pools/Teleportation.json';
+
+export default [
+  Concealment,
+  Fighting,
+  Flight,
+  Force_of_Will,
+  Leadership,
+  Leaping,
+  Medicine,
+  Presence,
+  Sorcery,
+  Speed,
+  Teleportation,
+];
