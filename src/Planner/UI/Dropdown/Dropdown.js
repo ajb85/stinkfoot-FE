6a189ee7ddc5f58ee042387ed86910679291dd82
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-04-30 17:50:46 -0400

    Adds a component to handle all dropdown menus

---
+++ src/Planner/UI/Dropdown/Dropdown.js
@@ -0,0 +1,62 @@
+import React, { useContext, useEffect } from 'react';
+
+import styles from './styles.module.scss';
+import { DDTrackingContext } from 'Providers/DropdownTracking.js';
+
+function Dropdown(props) {
+  const { dropdowns, addDropdown, removeDropdown } = useContext(
+    DDTrackingContext
+  );
+  const selected = props.options.find(({ value }) => value === props.selected);
+  useEffect(() => {
+    addDropdown(props.name);
+
+    return removeDropdown.bind(this, props.name);
+    // eslint-disable-next-line
+  }, []);
+
+  const isToggled = dropdowns[props.name];
+
+  const renderItem = (item = selected) => {
+    return (
+      <>
+        {item.image && <img src={item.image} alt={item.alt || item.value} />}
+        <p>{item.value}</p>
+      </>
+    );
+  };
+
+  return (
+    <div className={styles.Dropdown} data-name={props.name}>
+      <div className={styles.select} style={{ width: props.width || 150 }}>
+        {renderItem()}
+      </div>
+      {isToggled && (
+        <div style={{ position: 'relative', zIndex: 1000 }}>
+          <div className={styles.options}>
+            {props.options
+              .filter(
+                ({ value }) => props.showSelected || value !== selected.value
+              )
+              .map((item) => (
+                <div
+                  onClick={(e) => {
+                    e.target.name = props.name;
+                    e.target.value = item.value;
+                    props.onChange(e);
+                  }}
+                  key={item.value}
+                  className={styles.option}
+                  style={{ width: props.width || 150 }}
+                >
+                  {renderItem(item)}
+                </div>
+              ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Dropdown;
