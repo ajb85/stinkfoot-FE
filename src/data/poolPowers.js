832adb8aec3e785984fc61e420b6a4c886ac34f0
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-04-30 13:14:49 -0400

    Simplifies power_pools folder to pools to be more consistent with other power set types

--- src/data/poolPowers.js
+++ src/data/poolPowers.js
@@ -1,14 +1,14 @@
-import Concealment from 'data/power_pools/Concealment.json';
-import Fighting from 'data/power_pools/Fighting.json';
-import Flight from 'data/power_pools/Flight.json';
-import Force_of_Will from 'data/power_pools/Force_of_Will.json';
-import Leadership from 'data/power_pools/Leadership.json';
-import Leaping from 'data/power_pools/Leaping.json';
-import Medicine from 'data/power_pools/Medicine.json';
-import Presence from 'data/power_pools/Presence.json';
-import Sorcery from 'data/power_pools/Sorcery.json';
-import Speed from 'data/power_pools/Speed.json';
-import Teleportation from 'data/power_pools/Teleportation.json';
+import Concealment from 'data/pools/Concealment.json';
+import Fighting from 'data/pools/Fighting.json';
+import Flight from 'data/pools/Flight.json';
+import Force_of_Will from 'data/pools/Force_of_Will.json';
+import Leadership from 'data/pools/Leadership.json';
+import Leaping from 'data/pools/Leaping.json';
+import Medicine from 'data/pools/Medicine.json';
+import Presence from 'data/pools/Presence.json';
+import Sorcery from 'data/pools/Sorcery.json';
+import Speed from 'data/pools/Speed.json';
+import Teleportation from 'data/pools/Teleportation.json';
 
 export default [
   Concealment,
