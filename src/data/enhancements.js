f72253aaaa30650ac98557a0fe1d821ef655f730
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-05-03 19:54:55 -0400

    Imports existing enhancements

---
+++ src/data/enhancements.js
@@ -0,0 +1,58 @@
+// Standard enhancements: IO, SO, DO, TO
+import Accuracy from './enhancements/standard/Accuracy.json';
+import Range from './enhancements/standard/Range.json';
+import Confuse from './enhancements/standard/Confuse.json';
+import Damage from './enhancements/standard/Damage.json';
+import Defense_Buff from './enhancements/standard/Defense_Buff.json';
+import Defense_DeBuff from './enhancements/standard/Defense_DeBuff.json';
+import Endurance_Modification from './enhancements/standard/Endurance_Modification.json';
+import Endurance_Discount from './enhancements/standard/Endurance_Discount.json';
+import Fear from './enhancements/standard/Fear.json';
+import Fly from './enhancements/standard/Fly.json';
+import Heal from './enhancements/standard/Heal.json';
+import Hold from './enhancements/standard/Hold.json';
+import Immobilize from './enhancements/standard/Immobilize.json';
+import Intangible from './enhancements/standard/Intangible.json';
+import Interrupt from './enhancements/standard/Interrupt.json';
+import Jump from './enhancements/standard/Jump.json';
+import Knockback from './enhancements/standard/Knockback.json';
+import Recharge from './enhancements/standard/Recharge.json';
+import Res_Damage from './enhancements/standard/Res_Damage.json';
+import Run from './enhancements/standard/Run.json';
+import Sleep from './enhancements/standard/Sleep.json';
+import Slow from './enhancements/standard/Slow.json';
+import Stun from './enhancements/standard/Stun.json';
+import Taunt from './enhancements/standard/Taunt.json';
+import ToHit_Buff from './enhancements/standard/ToHit_Buff.json';
+import ToHit_DeBuff from './enhancements/standard/ToHit_DeBuff.json';
+
+export default {
+  standard: {
+    Accuracy,
+    Range,
+    Confuse,
+    Damage,
+    Defense_Buff,
+    Defense_DeBuff,
+    Endurance_Modification,
+    Endurance_Discount,
+    Fear,
+    Fly,
+    Heal,
+    Hold,
+    Immobilize,
+    Intangible,
+    Interrupt,
+    Jump,
+    Knockback,
+    Recharge,
+    Res_Damage,
+    Run,
+    Sleep,
+    Slow,
+    Stun,
+    Taunt,
+    ToHit_Buff,
+    ToHit_DeBuff,
+  },
+};
