46333fd6215f1a1e2299c1b6d855b1ca6e7bff1c
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-05-03 19:51:31 -0400

    Renders enhancements instead of an empty powerslot once they've been added to a power

--- src/Planner/PowerSlots/PowerSlots.js
+++ src/Planner/PowerSlots/PowerSlots.js
@@ -5,26 +5,13 @@ import { PlannerContext } from 'Providers/PlannerStateManagement.js';
 import styles from './styles.module.scss';
 
 function PowerSlots(props) {
-  const stateManager = React.useContext(PlannerContext);
-  const {
-    updateTracking,
-    togglePowerSlot,
-    addSlot,
-    removeSlot,
-    getPower,
-  } = stateManager;
-  let index = 0;
+  const enhNavigation = React.useState({
+    section: 'standard',
+    tier: 'IO',
+  });
 
-  const handlePillClick = (e, psIndex) => {
-    const { className } = e.target;
-    console.log('CLASS NAME: ', className);
-    if (
-      className === 'pillText' ||
-      stateManager.toggledPowerSlotIndex !== psIndex
-    ) {
-      togglePowerSlot(psIndex);
-    }
-  };
+  const stateManager = React.useContext(PlannerContext);
+  let index = 0;
 
   const { selected /*, defaults*/ } = stateManager
     .getFromState('powerSlots')
@@ -54,81 +41,18 @@ function PowerSlots(props) {
       {selected.map((column, columnNumber) => (
         <div key={columnNumber} className={styles.column}>
           {column.map((powerSlot) => {
-            const { level, power, enhSlots, originalIndex } = powerSlot;
-            const isActive = stateManager.activeLevel === level;
+            const { originalIndex } = powerSlot;
             const isEmpty = !powerSlot.power;
-            const isToggled =
-              stateManager.toggledPowerSlotIndex === originalIndex;
-            const p = power ? getPower(power) : {};
 
             return (
               <Fragment key={originalIndex}>
                 {isEmpty ? (
-                  <EmptyPowerSlot
-                    updateTracking={updateTracking}
-                    index={originalIndex}
-                    isActive={isActive}
-                    level={powerSlot.level}
-                  />
+                  <EmptyPowerSlot render={powerSlot} />
                 ) : (
-                  <div
-                    className={styles.powerContainer}
-                    onClick={(e) => handlePillClick(e, originalIndex)}
-                    name="Pill"
-                  >
-                    <div
-                      className={`${styles.power} ${
-                        isToggled && styles.toggled
-                      }`}
-                      style={{
-                        backgroundColor: '#1b4ea8',
-                        zIndex:
-                          stateManager.getFromState('powerSlots').length -
-                          originalIndex,
-                      }}
-                    >
-                      <p className="pillText">
-                        ({level}) {p.displayName}
-                      </p>
-
-                      <div className={styles.enhancementsContainer}>
-                        {enhSlots.map(({ slotLevel, setName, name }, j) => {
-                          const displayLevel =
-                            slotLevel === null ? level : slotLevel;
-                          return (
-                            <div
-                              key={`${originalIndex} ${j}`}
-                              onClick={removeSlot.bind(this, originalIndex, j)}
-                              className={styles.enhancementBubble}
-                            >
-                              <p>{displayLevel}</p>
-                            </div>
-                          );
-                        })}
-                      </div>
-
-                      {isToggled && (
-                        <div className={styles.addEnhancements}>
-                          <p onClick={addSlot.bind(this, originalIndex)}>+</p>
-                          <div className={styles.enhancementList}>
-                            {stateManager
-                              .getEnhancementsForPowerByType(p, 'IO')
-                              .map(
-                                ({ image: { enhancement, overlay }, name }) => (
-                                  <div
-                                    key={name}
-                                    className={styles.enhancementImage}
-                                  >
-                                    <img src={overlay} alt={name} />
-                                    <img src={enhancement} alt={name} />
-                                  </div>
-                                )
-                              )}
-                          </div>
-                        </div>
-                      )}
-                    </div>
-                  </div>
+                  <PowerSlot
+                    render={powerSlot}
+                    selectionState={enhNavigation}
+                  />
                 )}
               </Fragment>
             );
@@ -139,19 +63,180 @@ function PowerSlots(props) {
   );
 }
 
-function EmptyPowerSlot({ index, level, isActive, updateTracking }) {
+function EmptyPowerSlot({ render }) {
+  const stateManager = React.useContext(PlannerContext);
+  const { level, originalIndex } = render;
+  const isActive = stateManager.activeLevel === level;
+
   return (
     <div className={styles.powerContainer}>
       <div
         className={styles.power}
         onClick={(e) => {
           e.target.name = 'activeLevelIndex';
-          e.target.value = index;
-          updateTracking.bind(this, e);
+          e.target.value = originalIndex;
+          stateManager.updateTracking(e);
         }}
         style={{ backgroundColor: isActive ? 'green' : 'grey' }}
       >
         <p>({level})</p>
+      </div>
+    </div>
+  );
+}
+
+function PowerSlot({ render, selectionState }) {
+  const [enhNavigation, setEnhNavigation] = selectionState;
+  const stateManager = React.useContext(PlannerContext);
+  const { getPower } = stateManager;
+  const { level, power, enhSlots, originalIndex } = render;
+  const isToggled = stateManager.toggledPowerSlotIndex === originalIndex;
+  const p = power ? getPower(power) : {};
+  const zIndex =
+    stateManager.getFromState('powerSlots').length * 2 - originalIndex * 2;
+  const { togglePowerSlot, removeSlot } = stateManager;
+
+  const overlayImg = stateManager.getEnhancementOverlay(enhNavigation.tier);
+
+  const handlePillClick = (e, psIndex) => {
+    const { className } = e.target;
+    if (
+      className === 'pillText' ||
+      stateManager.toggledPowerSlotIndex !== psIndex
+    ) {
+      togglePowerSlot(psIndex);
+    }
+  };
+
+  return (
+    <div
+      className={styles.powerContainer}
+      onClick={(e) => handlePillClick(e, originalIndex)}
+    >
+      <div
+        className={`${styles.power} ${isToggled && styles.toggled}`}
+        style={{
+          backgroundColor: '#1b4ea8',
+          zIndex,
+        }}
+      >
+        <p className="pillText">
+          ({level}) {p.displayName}
+        </p>
+
+        <div className={styles.selectEnhancements}>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              padding: '0 9px',
+            }}
+          >
+            <p
+              onClick={() => setEnhNavigation({ ...enhNavigation, tier: 'IO' })}
+              style={{
+                color: enhNavigation.tier === 'IO' ? 'red' : null,
+                cursor: 'pointer',
+              }}
+            >
+              IO
+            </p>
+            <p
+              onClick={() => setEnhNavigation({ ...enhNavigation, tier: 'SO' })}
+              style={{
+                color: enhNavigation.tier === 'SO' ? 'red' : null,
+                cursor: 'pointer',
+              }}
+            >
+              SO
+            </p>
+            <p
+              onClick={() => setEnhNavigation({ ...enhNavigation, tier: 'DO' })}
+              style={{
+                color: enhNavigation.tier === 'DO' ? 'red' : null,
+                cursor: 'pointer',
+              }}
+            >
+              DO
+            </p>
+            <p
+              onClick={() => setEnhNavigation({ ...enhNavigation, tier: 'TO' })}
+              style={{
+                color: enhNavigation.tier === 'TO' ? 'red' : null,
+                cursor: 'pointer',
+              }}
+            >
+              TO
+            </p>
+          </div>
+          <div className={styles.enhPreviewList}>
+            {stateManager
+              .getEnhancementSectionForPower(p, enhNavigation.section)
+              .map((enh) => (
+                <div className={styles.enhPreview}>
+                  <div
+                    key={enh.fullName}
+                    className={styles.enhancementImage}
+                    onClick={stateManager.addEnhancement.bind(
+                      this,
+                      originalIndex,
+                      enh,
+                      enhNavigation.tier,
+                      50
+                    )}
+                  >
+                    <img src={overlayImg} alt={enh.fullName} />
+                    <img src={enh.image} alt={enh.fullName} />
+                  </div>
+                </div>
+              ))}
+          </div>
+        </div>
+      </div>
+      <div
+        style={{ zIndex: zIndex + 1 }}
+        className={styles.enhancementsContainer}
+      >
+        {enhSlots.map(({ slotLevel, enhancement }, j) => {
+          const displayLevel = slotLevel === null ? level : slotLevel;
+
+          const images = enhancement
+            ? stateManager.getEnhancementAndOverlayImages(enhancement)
+            : null;
+          return (
+            <>
+              {images ? (
+                <div
+                  className={styles.enhancementSlot}
+                  key={enhancement.displayName}
+                  onClick={stateManager.removeSlot.bind(this, originalIndex, j)}
+                >
+                  <div
+                    className={styles.enhancementImage}
+                    style={{ left: 8, top: 0 }}
+                  >
+                    <img src={images.overlay} alt={enhancement.displayName} />
+                    <img
+                      src={images.enhancement}
+                      alt={enhancement.displayName}
+                    />
+                    <p>{displayLevel}</p>
+                  </div>
+                </div>
+              ) : (
+                <div className={styles.enhancementSlot}>
+                  <div
+                    key={`${originalIndex} ${j}`}
+                    onClick={removeSlot.bind(this, originalIndex, j)}
+                    className={styles.enhancementBubble}
+                  >
+                    <p>{displayLevel}</p>
+                  </div>
+                </div>
+              )}
+            </>
+          );
+        })}
       </div>
     </div>
   );
