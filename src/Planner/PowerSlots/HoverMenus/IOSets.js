a577bd8c9848ba5dca440c1b4e12f8ad8eee0d78
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-05-23 21:29:33 -0400

    Abstracts sub sections into components to be re-used

--- src/Planner/PowerSlots/HoverMenus/IOSets.js
+++ src/Planner/PowerSlots/HoverMenus/IOSets.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import { usePlannerState } from 'Providers/PlannerStateManagement.js';
+import SetBonuses from './SetBonuses.js';
+import EnhancementList from './EnhancementList.js';
 
 import styles from '../styles.module.scss';
 
@@ -8,12 +10,6 @@ export default function SetPreviewMenu(props) {
   const stateManager = usePlannerState();
   const { display, powerSlotIndex, enhNavigation } = props;
   const { displayName, enhancements, setTypeName, levels } = props.set;
-  const setBonuses = stateManager.getDisplayBonuses(displayName, enhNavigation);
-
-  const bonusTier =
-    stateManager.getBonusTiersForPowerSlot(powerSlotIndex)[
-      enhancements[0].setIndex
-    ] || 0;
 
   return (
     <div className={styles.EnhHoverMenu} style={{ display }}>
@@ -27,112 +23,45 @@ export default function SetPreviewMenu(props) {
           <h2>
             {displayName}, {setTypeName}
           </h2>
-          {/* <h3>Set Type: {setTypeName}</h3> */}
           <h3>{parseLevels(levels)}</h3>
         </div>
-        <div className={styles.hoverContainer}>
-          <h3
-            className={styles.addFullSet}
-            onClick={stateManager.addFullEnhancementSet.bind(
-              this,
-              powerSlotIndex,
-              enhNavigation,
-              enhancements[0].setIndex,
-              50
-            )}
-          >
-            Add Full Set
-          </h3>
-          {/* <h3>Enhancements</h3> */}
-          <div className={styles.enhancementContainer}>
-            {enhancements.map((enh) => {
-              const enhancementIndex = stateManager.findEnhancementIndex(
-                enh,
-                powerSlotIndex
-              );
 
-              const isAdded = enhancementIndex > -1;
-
-              const canBeAdded = !isAdded
-                ? stateManager.canEnhancementGoInPowerSlot(enh, powerSlotIndex)
-                : null;
-
-              const c = styles.enhancementPill;
-              const className = isAdded
-                ? c + ' ' + styles.activePill
-                : !canBeAdded
-                ? c + ' ' + styles.inactivePill
-                : c;
-
-              return (
-                <p
-                  key={enh.displayName}
-                  onClick={
-                    !isAdded && canBeAdded
-                      ? stateManager.addEnhancement.bind(
-                          this,
-                          powerSlotIndex,
-                          enh,
-                          enhNavigation,
-                          50
-                        )
-                      : stateManager.removeSlots.bind(
-                          this,
-                          powerSlotIndex,
-                          enhancementIndex
-                        )
-                  }
-                  className={className}
-                >
-                  {stateManager.shortenEnhName(enh.displayName)}
-                </p>
-              );
-            })}
-          </div>
-        </div>
-        <div className={styles.hoverContainer}>
-          <h3>Set Bonuses</h3>
-          {setBonuses.reduce((acc, b, bonusIndex) => {
-            const { unlocked, bonusName, displays } = b;
-            const bonusCount = stateManager.getBonusCount(bonusName);
-            const willGetBonus = bonusCount < 5;
-            const isBonusUnlocked = unlocked <= bonusTier;
-            const bonusColor = {
-              color: isBonusUnlocked
-                ? bonusCount === 5
-                  ? 'chartreuse'
-                  : bonusCount > 5
-                  ? 'red'
-                  : 'gold'
-                : willGetBonus
-                ? null
-                : 'grey',
-              textDecoration:
-                !isBonusUnlocked && !willGetBonus ? 'line-through' : null,
-            };
-
-            acc.push(
-              <div key={bonusIndex} className={styles.bonusContainer}>
-                <p style={bonusColor}>{`${
-                  bonusCount > 0 ? `x${bonusCount}` : ''
-                }`}</p>
-                <p style={bonusColor}>({unlocked})</p>
-                <div className={styles.bonusText}>
-                  {displays.map((display) => (
-                    <p style={bonusColor} key={display}>
-                      {display}
-                    </p>
-                  ))}
-                </div>
-              </div>
-            );
-            return acc;
-          }, [])}
-        </div>
+        <EnhancementList
+          enhancements={enhancements}
+          powerSlotIndex={powerSlotIndex}
+          addEnh={addEnh(stateManager, powerSlotIndex, enhNavigation)}
+          addFull={addFull(
+            stateManager,
+            powerSlotIndex,
+            enhNavigation,
+            enhancements[0].setIndex
+          )}
+          removeEnh={removeEnh((stateManager, powerSlotIndex))}
+        />
+        <SetBonuses
+          set={props.set}
+          powerSlotIndex={powerSlotIndex}
+          enhNavigation={enhNavigation}
+        />
       </div>
     </div>
   );
 }
+
+const addEnh = (stateManager, powerSlotIndex, enhNavigation, level) => (enh) =>
+  stateManager.addEnhancement(powerSlotIndex, enh, enhNavigation, level || 50);
+
+const addFull = (stateManager, powerSlotIndex, enhNavigation, setIndex) =>
+  stateManager.addFullEnhancementSet.bind(
+    this,
+    powerSlotIndex,
+    enhNavigation,
+    setIndex,
+    50
+  );
+
+const removeEnh = (stateManager, powerSlotIndex) => (enhIndex) =>
+  stateManager.removeSlots(powerSlotIndex, enhIndex);
 
 const parseLevels = ({ min, max }) => {
   return min === max ? `Level ${min}` : `Levels: ${min} - ${max}`;
---
+++ src/Planner/PowerSlots/HoverMenus/SetBonuses.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import { usePlannerState } from 'Providers/PlannerStateManagement.js';
+
+import styles from '../styles.module.scss';
+
+function SetBonuses({ set, powerSlotIndex, enhNavigation }) {
+  const stateManager = usePlannerState();
+  const { enhancements, displayName } = set;
+  const setBonuses = stateManager.getDisplayBonuses(displayName, enhNavigation);
+
+  const bonusTier =
+    stateManager.getBonusTiersForPowerSlot(powerSlotIndex)[
+      enhancements[0].setIndex
+    ] || 0;
+  return (
+    <div className={styles.hoverContainer}>
+      <h3>Set Bonuses</h3>
+      {setBonuses.reduce((acc, b, bonusIndex) => {
+        const { unlocked, bonusName, displays } = b;
+        const bonusCount = stateManager.getBonusCount(bonusName);
+        const willGetBonus = bonusCount < 5;
+        const isBonusUnlocked = unlocked <= bonusTier;
+        const bonusColor = {
+          color: isBonusUnlocked
+            ? bonusCount === 5
+              ? 'chartreuse'
+              : bonusCount > 5
+              ? 'red'
+              : 'gold'
+            : willGetBonus
+            ? null
+            : 'grey',
+          textDecoration:
+            !isBonusUnlocked && !willGetBonus ? 'line-through' : null,
+        };
+
+        acc.push(
+          <div key={bonusIndex} className={styles.bonusContainer}>
+            <p style={bonusColor}>{`${
+              bonusCount > 0 ? `x${bonusCount}` : ''
+            }`}</p>
+            <p style={bonusColor}>({unlocked})</p>
+            <div className={styles.bonusText}>
+              {displays.map((display) => (
+                <p style={bonusColor} key={display}>
+                  {display}
+                </p>
+              ))}
+            </div>
+          </div>
+        );
+        return acc;
+      }, [])}
+    </div>
+  );
+}
+
+export default SetBonuses;
