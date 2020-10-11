71b210261cf93ca79e6317a319c092153bde6468
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-10-10 20:28:38 -0400

    refactor: rewrites dropdown/modal opening and closing as a wrapper

--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Routes from './components/Routes/';
+import React from "react";
+import ReactDOM from "react-dom";
+import Routes from "./components/Routes/";
 
 // Context
-import Providers from 'Providers/';
+import Providers from "Providers/";
 
 // Routing
-import { Router } from 'react-router-dom';
-import history from 'history.js';
+import { Router } from "react-router-dom";
+import history from "history.js";
 
 // Styles
-import './styles/index.scss';
-import 'faLibrary.js';
+import "./styles/index.scss";
+
+import { closeMenu } from "js/closeTracker.js";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router history={history}>
       <Providers>
-        <Routes />
+        <div onClick={closeMenu}>
+          <Routes />
+        </div>
       </Providers>
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
