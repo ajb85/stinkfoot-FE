3f5d879a257b239afa45a0676930239f78aada9a
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-10-29 17:23:22 -0400

    Moves logic from state manager into helper files

---
+++ src/js/shortenEnhName.js
@@ -0,0 +1,19 @@
+const shortened = {
+  Accuracy: "Acc",
+  Damage: "Dmg",
+  Defense: "Def",
+  Endurance: "End",
+  "Fly Speed": "FlySpd",
+  Healing: "Heal",
+  "Hit Points": "HP",
+  "Jump Speed": "JumpSpd",
+  Recharge: "Rech",
+  "Run Speed": "RunSpd",
+};
+
+export default (name) => {
+  return name
+    .split("/")
+    .map((n) => (shortened[n] ? shortened[n] : n))
+    .join("/");
+};
