3671c07f77ef7ee20b1398b38186e1f5da3f42fc
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-10-29 20:36:22 -0400

    Adds logic to accept powerslots and loop over them to build reference/lookup objects

--- src/helpers/analyzeBuild.js
+++ src/helpers/analyzeBuild.js
@@ -1,20 +1,99 @@
-const initial = {
-  powersLookup: {},
-  enhancementsLookup: {},
-  excludedPowers: {},
-  excludedPowersets: {},
-};
+import { getPower } from "./powersets.js";
+import { getEnhancement } from "./enhancements.js";
 
-export default ((cache) => (build) => {
-  if (cache.build === build) {
+export default ((cache) => (powerSlots, archetype, activeSets) => {
+  if (cache.build === powerSlots) {
     return cache.results;
   }
 
-  // analyze build
-  const results = initial;
+  const reduceSlot = getSlotReducer(archetype);
+  const results = powerSlots.reduce(reduceSlot, getInitialAcc());
+  evaluateSets(activeSets, results);
 
-  cache.build = build;
+  cache.build = powerSlots;
   cache.results = results;
-
   return results;
 })({});
+
+function getSlotReducer(archetype) {
+  let accumulator;
+  let currentPower;
+  let currentIndex;
+  function evaluateEnhSlot({ enhancement }) {
+    if (!enhancement) {
+      return;
+    }
+    // enhancement -> displayName, fullName, imageName, level, setIndex (set only), tier, type
+    const e = getEnhancement(enhancement);
+    if (e.isUnique) {
+      // Record any existing unique enhancements
+      accumulator.excluded.enhancements[e.fullName] = true;
+    }
+
+    if (!accumulator.lookup.enhancements[e.fullName]) {
+      // If enhancement is new, setup initial state
+      accumulator.lookup.enhancements[e.fullName] = {};
+    }
+
+    const record = accumulator.lookup.enhancements[e.fullName];
+    const recordOfEnh = record[currentPower.fullName];
+
+    if (recordOfEnh) {
+      // Increment its count if power is already added for this enhancement
+      recordOfEnh.count++;
+      recordOfEnh.powerSlotIndices.push(currentIndex);
+    } else {
+      // Create the record for a new power for this enhancement
+      recordOfEnh[currentPower.fullName] = {
+        powerDisplayName: currentPower.displayName,
+        powerSlotIndices: [currentIndex],
+        count: 1,
+      };
+    }
+  }
+
+  return function reduceSlot(acc, { enhSlots, level, power, type }, i) {
+    const isEmpty = !power || !enhSlots;
+    if (isEmpty) {
+      return acc;
+    }
+
+    // Save power in look up
+    currentPower = getPower(power, archetype);
+    currentIndex = i;
+    acc.lookup.powers[currentPower.fullName] = i;
+
+    // Save enhancements in look up
+    accumulator = acc;
+    enhSlots.forEach(evaluateEnhSlot);
+
+    return acc;
+  };
+}
+
+function getInitialAcc() {
+  return {
+    lookup: { powers: {}, enhancements: {} },
+    excluded: { powers: {}, powersets: {}, enhancements: {} },
+  };
+}
+
+function evaluateSets({ primary, secondary, pools }, acc) {
+  const primaryPrevents = primary.prevents || [];
+  const secondaryPrevents = secondary.prevents || [];
+  const poolsPrevents = pools.reduce((acc, { prevents }) => {
+    if (prevents) {
+      return [...acc, ...prevents];
+    }
+
+    return acc;
+  });
+
+  [...primaryPrevents, ...secondaryPrevents, ...poolsPrevents].reduce(
+    (acc, ex) => {
+      acc[ex] = true;
+      return acc;
+    },
+    acc.excluded.powersets
+  );
+}
