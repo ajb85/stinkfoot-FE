4d84f627282f71e1bd2fcdcf10733da878bf8cad
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-11-11 20:56:33 -0500

    Fixes overflow on slide menu

--- src/components/SlideDropdown/SlideDropdown.js
+++ src/components/SlideDropdown/SlideDropdown.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
 import styles from "./styles.module.scss";
 
+let timeout;
 function SlideDropdown(props) {
+  const dropdown = useRef();
+
+  useEffect(() => {
+    clearTimeout(timeout);
+    if (dropdown.current) {
+      if (props.isToggled) {
+        timeout = setTimeout(() => {
+          dropdown.current.style.overflow = "visible";
+        }, 250);
+      } else {
+        dropdown.current.style.overflow = "hidden";
+      }
+    }
+  }, [props.isToggled]);
+
   return (
     <div
+      ref={dropdown}
       className={
         styles.SlideDropdown + (props.isToggled ? " " + styles.toggled : "")
       }
