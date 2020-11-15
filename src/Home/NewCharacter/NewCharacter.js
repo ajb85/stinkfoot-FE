c435c72dfb643b402590d6156b6d81826fe2088b
AJ Brush <>
2020-11-15 12:35:36 -0500

    Basic character creation & form validation

--- src/Home/NewCharacter/NewCharacter.js
+++ src/Home/NewCharacter/NewCharacter.js
@@ -1,9 +1,19 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { Form, Button, Modal, ModalBody, ModalHeader } from "shards-react";
+import {
+  Alert,
+  Form,
+  FormInput,
+  Button,
+  Modal,
+  ModalBody,
+  ModalHeader,
+} from "shards-react";
 
-import Input from "components/Input/";
+// import Input from "components/Input/";
 // import Button from "components/Button/";
+
+import useCharacters from "providers/useCharacters.js";
 
 import styles from "./styles.module.scss";
 
@@ -38,17 +48,43 @@ export default function AddCharacter(props) {
 
 function NewCharacter() {
   const [charName, setCharName] = useState("");
+  const { characters, createNewCharacter } = useCharacters();
   const updateName = (e) => setCharName(e.target.value);
 
-  const onSubmit = (e) => e.preventDefault();
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (charName.length && !characters[charName]) {
+      createNewCharacter(charName);
+      setCharName("");
+    }
+  };
+
   return (
     <Form onSubmit={onSubmit}>
-      <Input
-        value={charName}
-        onChange={updateName}
-        placeholder="Character Name"
-      />
-      <Button submit>+</Button>
+      <h2>
+        Create a new character from scratch within the app. If you'd like to
+        import a character from Mids or Pines, use the Import button at the top.
+      </h2>
+      <div className={styles.newCharacterInput}>
+        <FormInput
+          valid={charName.length && !characters[charName]}
+          inline
+          value={charName}
+          onChange={updateName}
+          placeholder="Character Name"
+        />
+        <Button
+          disabled={!charName.length || characters[charName]}
+          type="submit"
+        >
+          Create
+        </Button>
+      </div>
+      {characters[charName] && (
+        <Alert className={styles.Alert} theme="danger">
+          You already have a character with that name.
+        </Alert>
+      )}
     </Form>
   );
 }
